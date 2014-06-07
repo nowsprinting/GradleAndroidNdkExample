55da50aeb386ae8b3534997eb29e7610e855aeec
Koji Hasegawa <hasegawa@hubsys.co.jp>
2014-06-07 15:11:15 +0900

    Change gradle plugin version to 0.11.0
    *Details:
    - Android SDK Tools r22.6.4
    - Android SDK Platform-tools r19.0.2
    - Android SDK Build-tools r19.1
    - Android SDK 4.4.2(API level 19)
    - Android NDK r9d(March 2014)
    - Gradle 1.12 + Gradle plugin for Android 0.11.0
    
    *etc:
    - Android Studio 0.6.0

--- jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := cppmodule
-LOCAL_SRC_FILES := CppExample.cpp CppExample_wrap.cpp 
-
-include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI         := armeabi-v7a
-APP_STL         := gnustl_static
-APP_PLATFORM    := android-8
